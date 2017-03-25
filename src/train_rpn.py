5a536a4f9eb3a772681fdbc1bdda14c5830e2deb
Hao Nguyen <hao.nguyen1214@gmail.com>
2017-03-24 18:21:55 -0700

    voc dataset completed

--- src/train_rpn.py
+++ src/train_rpn.py
@@ -2,14 +2,16 @@ from os import listdir
 from os.path import join
 
 import torch
-from torch.autograd import Variable
 import torch.optim as opt
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
 
 import numpy as np
 import numpy.random as npr
 import pandas as pd
 from PIL import Image
 
+from voc_dataset import VOCDataSet, collate_fn
 from region_proposal import RegionProposalNetwork, get_target_weights
 from smooth_l1_loss import SmoothL1Loss
 from generate_anchor_data import AnchorDataGenerator
@@ -17,55 +19,34 @@ from generate_anchor_data import AnchorDataGenerator
 
 EPOCH = 1
 
-DATA_DIR = "data"
-IMAGE_DIR = join(DATA_DIR, "JPEGImages")
-ANNOTATION_DIR = join(DATA_DIR, "Preprocess/Annotations")
-
-
-def get_image_input(image_path):
-    image = Image.open(image_path)
-    image_a = np.asarray(image).astype(np.float32).transpose(2, 0, 1)
-    image_a = image_a[np.newaxis, :]
-
-    input = Variable(torch.Tensor(image_a))
-    im_h = Variable(torch.Tensor([image_a.shape[2]]))
-    im_w = Variable(torch.Tensor([image_a.shape[3]]))
-
-    return input, im_w, im_h
-
-
-def get_ground_truth_boxes(annotation_path):
-    df = pd.read_csv(annotation_path)
-    gt = df.as_matrix()
-    gt = gt[:, 0:4]
-    return Variable(torch.Tensor(gt))
-
 if __name__ == "__main__":
-    image_files = [filename for filename in listdir(IMAGE_DIR)]
-
-    image_paths = [
-        (join(IMAGE_DIR, filename), join(ANNOTATION_DIR, filename + ".csv"))
-        for filename in image_files]
-
     rpn = RegionProposalNetwork()
     anchor_generator = AnchorDataGenerator()
 
+    #  classification loss
     log_softmax_func = torch.nn.LogSoftmax()
     nll_loss_func = torch.nn.NLLLoss2d(weight=get_target_weights(), size_average=False)
 
+    #  regression loss 
     regression_loss_func = SmoothL1Loss()
 
     optimizer = opt.SGD(rpn.parameters(), lr=0.001, momentum=0.9)
 
+    train_data = VOCDataSet("data", "train")
+    dataloader = DataLoader(train_data,
+                            batch_size=1,
+                            shuffle=True,
+                            num_workers=5,
+                            collate_fn=collate_fn 
+                           )
+
     for epoch in range(EPOCH):
-        indices = np.arange(len(image_paths))
-        npr.shuffle(indices)
-        indices = indices.tolist()
-       
-        for i in indices:
-            image_path, annotation_path = image_paths[i]
-            image, im_w, im_h = get_image_input(image_path)
-            ground_truth_boxes = get_ground_truth_boxes(annotation_path)
+        for image, ground_truth_boxes in dataloader:
+            image = Variable(torch.Tensor(image))
+            im_h = Variable(torch.Tensor([image.size(2)]))
+            im_w = Variable(torch.Tensor([image.size(3)]))
+
+            ground_truth_boxes = Variable(torch.Tensor(ground_truth_boxes))
 
             optimizer.zero_grad()
 
@@ -94,3 +75,5 @@ if __name__ == "__main__":
             loss = (nll_loss / 256.) + regression_loss
             loss.backward()
             optimizer.step()
+
+            print(loss.data.numpy())
---
+++ src/voc_dataset.py
@@ -0,0 +1,61 @@
+from os import listdir
+from os.path import join
+
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+
+import pandas as pd
+import numpy as np
+from PIL import Image
+
+
+PIXEL_MEANS = np.array([[[0.48462227599918]],
+                          [[0.45624044862054]],
+                          [[0.40588363755159]]])
+
+PIXEL_STDS = np.array([[[0.22889466674951]],
+                        [[0.22446679341259]],
+                        [[0.22495548344775]]])
+
+def collate_fn(batch):
+    image = batch[0][0][np.newaxis, :]
+    gt_boxes = batch[0][1]
+    return torch.Tensor(image), torch.Tensor(gt_boxes)
+
+class VOCDataSet(Dataset):
+
+    def __init__(self, root, image_set, include_gt_lable=False, download=False):
+        self._root = root
+        self._image_dir = join(self._root, "JPEGImages")
+        self._annotation_dir = join(self._root, "Preprocess/Annotations")
+        
+        #  get image file name
+        assert (image_set == "train") | (image_set == "val"), "Invalid image set"
+        with open(join(self._root, "ImageSets/Main/%s.txt" % image_set), "r") as f:
+            #  remove \n character
+            self._dataset_index = {i:line[0:-1] for i, line in enumerate(f)}
+
+        self._include_gt_label = include_gt_lable
+
+    def __getitem__(self, i):
+        image_path = join(self._image_dir, "%s.jpg" % self._dataset_index[i])
+        annotation_path = join(self._annotation_dir, "%s.jpg.csv" % self._dataset_index[i])
+
+        image = Image.open(image_path).convert("RGB")
+        image = np.asarray(image).astype(np.float).transpose(2, 0, 1)
+
+        #  image normalization
+        image = image / 255.0
+        image = (image - PIXEL_MEANS) / PIXEL_STDS
+
+        #  parse annotation file
+        annotations = pd.read_csv(annotation_path)
+        annotations = annotations.as_matrix()
+        if not self._include_gt_label:
+            annotations = annotations[:, 0:4]
+
+        return image, annotations
+
+    def __len__(self):
+        return len(self._dataset_index)

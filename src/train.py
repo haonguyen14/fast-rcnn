3e9cde2884e2bb2406af06f3611912bb06f3d926
hnguyen <hao.nguyen1214@gmail.com>
2017-05-01 18:40:09 -0700

    - added fast-rcnn and training code - added proposal generation - added notebook file to test fast-rcnn detection - improved voc dataset to support proposals

---
+++ src/train.py
@@ -0,0 +1,138 @@
+import argparse
+
+from os import makedirs
+from os.path import join, exists
+from shutil import rmtree
+
+import torch
+import torch.optim as opt
+from torch.nn import SmoothL1Loss, CrossEntropyLoss
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+
+from voc_dataset_2 import VOCDataSetROIs, collate_rois_fn
+from faster_rcnn import FasterRCNN
+from average import AverageMeter
+
+import time
+
+arguments = argparse.ArgumentParser(description="Training Fast-RCNN")
+arguments.add_argument("name", help="experiment name")
+arguments.add_argument("epoch", help="number of epoches", type=int)
+arguments.add_argument("--roi_path", help="roi data", default="data/ROIs")
+arguments.add_argument("--batch-size", help="batch size", type=int, default=2)
+arguments.add_argument("--rois-per-batch", help="number of rois per batch", type=int, default=128)
+arguments.add_argument("-e", "--experiments", help="experiment path", default="data/Experiments")
+arguments.add_argument("-d", "--display", help="display interval", type=int, default=100)
+arguments.add_argument("-r", "--resume", help="resume from a checkpoint", default=None)
+arguments.add_argument("--lr", help="base learning rate", type=float, default=0.001)
+arguments.add_argument("--momentum", help="momentum constant", type=float, default=0.9)
+arguments.add_argument("--gamma", help="learning rate decay constant", type=float, default=0.1)
+arguments.add_argument("--lr-stepsize", help="learning rate decay stepsize", type=int, default=11)
+arguments.add_argument("--replace", help="replace previous experiment with similar name", action="store_true")
+
+
+def create_experiment_dir():
+    experiment_dir = join(args.experiments, args.name)
+    checkpoint_dir = join(experiment_dir, "checkpoints")
+
+    if exists(experiment_dir) and args.replace:
+        rmtree(experiment_dir)
+    if exists(experiment_dir):
+        raise Exception("Experiment %s is already existed" % args.name)
+
+    makedirs(experiment_dir)
+    makedirs(checkpoint_dir)
+
+    return {
+        "main": experiment_dir,
+        "checkpoint": checkpoint_dir
+    }
+
+
+def adjust_learning_rate(optimizer, epoch):
+    lr = args.lr * (args.gamma ** (epoch // args.lr_stepsize))
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
+
+def main():
+    global args
+    args = arguments.parse_args()
+    experiment_env = create_experiment_dir()
+
+    assert args.rois_per_batch % args.batch_size == 0, "Uneven number of rois per image"
+    rois_per_image = args.rois_per_batch / args.batch_size
+    train_data = VOCDataSetROIs("data", "train", rois_per_image)
+    dataloader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=5, collate_fn=collate_rois_fn)
+
+    ################### MODEL BOOTSRAP #####################
+    print("[+] Bootstrapping model")
+    net = FasterRCNN(pretrained=True).cuda()
+    net.train(mode=True)
+
+    if args.resume is not None:
+        print("[+] Resuming from %s" % args.resume)
+        checkpoint = torch.load(args.resume)
+        net.load_state_dict(checkpoint["state_dict"])
+
+    cross_entropy = CrossEntropyLoss(size_average=True).cuda()
+    smooth_l1_loss = SmoothL1Loss(size_average=False).cuda()
+    optimizer = opt.SGD([params for params in net.parameters() if params.requires_grad],
+                        lr=args.lr, momentum=args.momentum, weight_decay=0.0005)
+
+    ################### MODEL TRAINING #####################
+    print("[+] Training model")
+    start_epoch = 0 if args.resume is None else checkpoint["epoch"]
+    for epoch in xrange(start_epoch, args.epoch):
+        adjust_learning_rate(optimizer, epoch)
+        train(net, cross_entropy, smooth_l1_loss, optimizer, dataloader, experiment_env, epoch)
+
+
+def train(net, cross_entropy, smooth_l1_loss, optimizer, dataloader, experiment, epoch):
+    epoch_start_time = time.time() 
+    epoch_loss = AverageMeter()
+    epoch_cls_loss = AverageMeter()
+    epoch_l1_loss = AverageMeter()
+
+    for i, (_, images, rois, roi_labels, regression_targets, regression_weights, image_info) in enumerate(dataloader):
+        optimizer.zero_grad()
+
+        images = Variable(images.cuda())
+        rois = Variable(rois.cuda())
+        roi_labels = Variable(roi_labels.cuda())
+        regression_targets = Variable(regression_targets.cuda())
+        regression_weights = Variable(regression_weights.cuda())
+
+        cls_scores, bbox_preds = net(images, rois)
+
+        # multi-class classification loss
+        cross_entropy_loss = cross_entropy(cls_scores, roi_labels)
+
+        # bbox classification loss
+        regression_targets = torch.mul(regression_targets, regression_weights)
+        bbox_preds = torch.mul(bbox_preds, regression_weights) 
+        bbox_loss = smooth_l1_loss(bbox_preds, regression_targets) / (regression_targets.size(0) + 1e-4)
+
+        loss = cross_entropy_loss + bbox_loss
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss.update(loss.data[0])
+        epoch_cls_loss.update(cross_entropy_loss.data[0])
+        epoch_l1_loss.update(bbox_loss.data[0])
+        if i % args.display == (args.display - 1):
+            print("Epoch %d/%d (step %d): ave_loss=%.3f cls_loss=%.3f l1_loss=%.3f" %
+                  (epoch + 1, args.epoch, i, epoch_loss.ave, epoch_cls_loss.ave, epoch_l1_loss.ave))
+
+    total_time = epoch_start_time - time.time()
+    print("Epoch %d/%d: ave_loss=%.3f total_time=%.3f" % (epoch + 1, args.epoch, epoch_loss.ave, total_time))
+    torch.save({
+        "epoch": epoch,
+        "ave_loss": epoch_loss.ave,
+        "state_dict": net.state_dict()
+    }, join(experiment["checkpoint"], "checkpoint_%d.tar" % epoch))
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/voc_dataset_2.py
@@ -0,0 +1,217 @@
+import os
+import cv2
+import numpy as np
+import numpy.random as npr
+import pandas as pd
+import pickle
+
+import torch
+from torch.utils.data import Dataset
+from anchor_util import *
+
+# BGR format to match VGG16 pretrained model
+PIXEL_MEANS = np.array([[[102.9801, 115.9465, 122.7717]]])
+INCLUDE_DIFFICULT_GT = False
+
+CLASSES = ('__background__',
+             'aeroplane', 'bicycle', 'bird', 'boat',
+             'bottle', 'bus', 'car', 'cat', 'chair',
+             'cow', 'diningtable', 'dog', 'horse',
+             'motorbike', 'person', 'pottedplant',
+             'sheep', 'sofa', 'train', 'tvmonitor')
+NUM_CLASSES = len(CLASSES)
+CLASS_TO_INDEX = dict(zip(CLASSES, xrange(NUM_CLASSES)))
+INDEX_TO_CLASS = dict(zip(xrange(NUM_CLASSES), CLASSES))
+
+ROI_BATCH_SIZE = 128
+FG_FRACTION = 0.25
+FG_THRESHOLD = 0.5
+BG_THRESHOLD_LOW = 0.1
+BG_THRESHOLD_HIGH = 0.5
+
+
+def create_roi_batch_data(batch_size, sample_bboxes, sample_labels, sample_regression_targets, sample_regression_weights):
+    bbox_indices = []
+    for i, bboxes in enumerate(sample_bboxes):
+        bbox_indices += ([i] * len(bboxes))
+    bbox_indices = np.array(bbox_indices, dtype=float).reshape(-1, 1)
+    
+    bboxes = np.hstack((bbox_indices, np.vstack(sample_bboxes))) 
+    labels = np.hstack(sample_labels)
+    regression_targets = np.vstack(sample_regression_targets)
+    regression_weights = np.vstack(sample_regression_weights)
+    return bboxes, labels, regression_targets, regression_weights
+
+
+def create_bbox_batch_data(batch_size, sample_bboxes, sample_labels):
+    #  generating batch index for each bbox
+    bbox_indices = []
+    for i, bboxes in enumerate(sample_bboxes):
+        bbox_indices += ([i] * len(bboxes))
+    bbox_indices = np.array(bbox_indices, dtype=float).reshape(-1, 1)
+    return np.hstack((bbox_indices, np.vstack(sample_bboxes))), np.hstack(sample_labels)
+
+
+def create_image_batch_data(batch_size, sample_images):
+    size = np.array([image.shape[1:] for image in sample_images]).max(axis=0)
+    batch = np.zeros((batch_size, 3, size[0], size[1]), dtype=np.float32)
+    for i, image in enumerate(sample_images):
+        batch[i, :, 0:image.shape[1], 0:image.shape[2]] = image
+    return batch
+
+
+def collate_rois_fn(samples):
+    batch_size = len(samples)
+    indices = np.array([s[0] for s in samples], dtype=np.int)
+    images = create_image_batch_data(batch_size, [s[1] for s in samples])
+    bboxes, labels, regression_targets, regression_weights  = create_roi_batch_data(
+            batch_size, [s[2] for s in samples], [s[3] for s in samples], [s[4] for s in samples], [s[5] for s in samples])
+    image_info = np.vstack([s[6] for s in samples])
+
+    return indices, \
+           torch.Tensor(images), \
+           torch.Tensor(bboxes), \
+           torch.LongTensor(labels.astype(np.long)), \
+           torch.Tensor(regression_targets), \
+           torch.Tensor(regression_weights), \
+           torch.Tensor(image_info)
+
+
+def collate_fn(samples):
+    batch_size = len(samples)
+    indices = np.array([s[0] for s in samples], dtype=np.int)
+    images = create_image_batch_data(batch_size, [s[1] for s in samples])
+    gt_bboxes, gt_labels = create_bbox_batch_data(batch_size, [s[2] for s in samples], [s[3] for s in samples])
+    image_info = np.vstack([s[4] for s in samples])
+
+    return indices, \
+           torch.Tensor(images), \
+           torch.Tensor(gt_bboxes), \
+           torch.Tensor(gt_labels), \
+           torch.Tensor(image_info)
+
+
+class VOCDataSet(Dataset):
+
+    def __init__(self, root, image_set, min_size=600, max_size=1000):
+        self._root = root
+        self._min_size = min_size
+        self._max_size = max_size
+        self._image_dir = os.path.join(self._root, "JPEGImages")
+        self._annotation_dir = os.path.join(self._root, "Preprocess/Annotations")
+        assert (image_set == "train") | (image_set == "val"), "Invalid image set"
+
+        with open(os.path.join(self._root, "ImageSets/Main/%s.txt" % image_set), "r") as f:
+            self._dataset_index = {i: line[0:-1] for i, line in enumerate(f)}
+
+    def _get_image(self, index):
+        image_path = os.path.join(self._image_dir, "%s.jpg" % self._dataset_index[index])
+        image = cv2.imread(image_path).astype(np.float32)
+        image = image - PIXEL_MEANS
+
+        #  resize image
+        shorter_dim = float(np.min(image.shape[0:2]))
+        longer_dim = float(np.max(image.shape[0:2]))
+        scale_const = float(self._min_size) / shorter_dim
+        if (scale_const * longer_dim) > self._max_size:
+            scale_const = float(self._max_size) / longer_dim
+        image = cv2.resize(image, None, None,
+                        fx=scale_const, fy=scale_const,
+                        interpolation=cv2.INTER_LINEAR)
+
+        #  transpose to match pytorch convolution input
+        image = image.transpose(2, 0, 1)
+        return image, np.hstack((image.shape[1:], scale_const)) 
+
+    def _get_annotation(self, index, image_info):
+        annotation_path = os.path.join(self._annotation_dir, "%s.jpg.csv" % self._dataset_index[index])
+        annotations = pd.read_csv(annotation_path)
+        annotations = annotations.as_matrix()
+        gt_idx = np.arange(0, annotations.shape[0])
+        if not INCLUDE_DIFFICULT_GT:
+            gt_idx = np.where(annotations[:, 4] == 0)[0]
+        return annotations[gt_idx, 0:4]*image_info[2], annotations[gt_idx, 5]
+
+    def get_image_name_from_index(self, index):
+        return self._dataset_index[index]
+
+    def __getitem__(self, i):
+        image, image_info = self._get_image(i)
+        gt_bbox, gt_label = self._get_annotation(i, image_info)
+        gt_bbox = gt_bbox.astype(np.float32)
+        gt_label = np.array([CLASS_TO_INDEX[x] for x in gt_label])
+        return i, image, gt_bbox, gt_label, image_info
+
+    def __len__(self):
+        return len(self._dataset_index)
+
+
+class VOCDataSetROIs(Dataset):
+
+    def __init__(self, root, image_set, rois_per_image, roi_path="data/ROIs", min_size=600, max_size=1000):
+        self._image_data = VOCDataSet(root, image_set, min_size, max_size)
+        self._roi_path = roi_path
+        self._rois_per_image = rois_per_image
+
+    def _get_rois(self, index, image_info):
+        image_name = self._image_data.get_image_name_from_index(index)
+        with open(os.path.join(self._roi_path, "%s.pkl" % image_name), "r") as f:
+            rois = pickle.load(f)
+        return rois * image_info[2]
+
+    def _get_overlap_classes(self, rois, gt_bbox, gt_label):
+        overlaps = get_overlap(rois, gt_bbox)
+        max_overlaps = overlaps.max(axis=1)
+        max_overlaps_idx = overlaps.argmax(axis=1)
+
+        overlap_classes = np.zeros((len(rois), NUM_CLASSES), dtype=np.float32)
+        roi_idx = np.where(max_overlaps > 0)[0]
+        overlap_classes[roi_idx, gt_label[max_overlaps_idx[roi_idx]]] = max_overlaps[roi_idx]
+        return overlap_classes, gt_label[max_overlaps_idx], gt_bbox[max_overlaps_idx]
+
+    def _get_bbox_regression_targets(self, targets, labels):
+        idx = np.where(labels > 0)[0] 
+        regression_targets = np.zeros((len(targets), 4*NUM_CLASSES), dtype=np.float32)
+        regression_weights = np.zeros(regression_targets.shape, dtype=np.float32)
+        for i in idx:
+            cls = labels[i]
+            start = 4 * cls
+            end = start + 4
+            regression_targets[i, start:end] = targets[i, :]
+            regression_weights[i, start:end] = np.array([1., 1., 1., 1.])
+        return regression_targets, regression_weights
+
+    def _sample_rois(self, rois, gt_bbox, gt_label, image_info):
+        overlap_classes, target_cls, target_gt = self._get_overlap_classes(rois, gt_bbox, gt_label)
+        assert len(overlap_classes) == len(target_cls) & len(overlap_classes) == len(target_gt)
+
+        fg_per_image = np.round(FG_FRACTION * self._rois_per_image)
+        fg_overlap_idx = np.where(overlap_classes >= FG_THRESHOLD)[0]
+        fg_this_image = int(np.minimum(len(fg_overlap_idx), fg_per_image))
+        if len(fg_overlap_idx) > 0:
+            fg_overlap_idx = npr.choice(fg_overlap_idx, size=fg_this_image, replace=False)
+
+        bg_overlap_idx = np.where((overlap_classes >= BG_THRESHOLD_LOW) & (overlap_classes < BG_THRESHOLD_HIGH))[0]
+        bg_this_image = self._rois_per_image - fg_this_image
+        bg_this_image = int(np.minimum(len(bg_overlap_idx), bg_this_image))
+        if len(bg_overlap_idx) > 0:
+            bg_overlap_idx = npr.choice(bg_overlap_idx, size=bg_this_image, replace=False)
+
+        indices = np.append(fg_overlap_idx, bg_overlap_idx)
+        keep_rois = rois[indices]
+        keep_labels = target_cls[indices]
+        keep_labels[fg_this_image:] = 0  # background class
+
+        bbox_regression_targets = compute_bbox_target(keep_rois, target_gt[indices])
+        bbox_regression_targets, bbox_regression_weights = self._get_bbox_regression_targets(bbox_regression_targets, keep_labels)
+
+        return keep_rois, keep_labels, bbox_regression_targets, bbox_regression_weights
+
+    def __getitem__(self, index):
+        _, image, gt_bbox, gt_label, image_info = self._image_data[index]
+        rois = self._get_rois(index, image_info)
+        rois, roi_labels, roi_bbox_targets, roi_bbox_weights = self._sample_rois(rois, gt_bbox, gt_label, image_info)
+        return index, image, rois, roi_labels, roi_bbox_targets, roi_bbox_weights, image_info
+
+    def __len__(self):
+        return len(self._image_data)
